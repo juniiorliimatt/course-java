2c6d9a75e0394e1bd05a1ceb76f45decd713b23b
Junior Lima <juniiorliimatt@gmail.com>
2021-06-09 23:18:31 -0300

    att

--- src/curso/entities/Student.java
+++ src/curso/entities/Student.java
@@ -6,26 +6,25 @@ public class Student {
     private final double noteTwo;
     private final double noteThree;
 
-    public Student(String name, double noteOne, double noteTwo, double noteThree) {
-        this.name = name;
-        this.noteOne = noteOne;
-        this.noteTwo = noteTwo;
-        this.noteThree = noteThree;
+    public Student( String name, double noteOne, double noteTwo, double noteThree ) {
+        this.name=name;
+        this.noteOne=noteOne;
+        this.noteTwo=noteTwo;
+        this.noteThree=noteThree;
     }
 
-    public double sum(){
-        return this.noteOne + this.noteTwo + this.noteThree;
+    public double sum() {
+        return this.noteOne+this.noteTwo+this.noteThree;
     }
 
-    public boolean isAproved(){
-        return sum() >= 60;
+    public boolean isAproved() {
+        return sum()>=60;
     }
 
-    public String finalGrade(){
-
-        if(isAproved()){
-            return "\n" + this.name + " \nFINAL GRADE = " + sum() + "\nPASS";
+    public String finalGrade() {
+        if ( isAproved() ) {
+            return "\n"+this.name+" \nFINAL GRADE = "+sum()+"\nPASS";
         }
-        return "\n" + this.name + " \nFINAL GRADE = " + sum() + "\nFAILED" + "\nMISSING " + (60 - sum()) + " POINTS";
+        return "\n"+this.name+" \nFINAL GRADE = "+ sum()+"\nFAILED"+"\nMISSING "+( 60-sum() )+" POINTS";
     }
 }
