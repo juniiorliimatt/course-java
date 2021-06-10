9bad63ba7161dabfdcfda2f8dfe11ea129cc3cb4
Junior Lima <juniiorliimatt@gmail.com>
2021-06-10 18:04:03 -0300

    atualizando a classe

--- src/curso/entities/Product.java
+++ src/curso/entities/Product.java
@@ -1,14 +1,39 @@
 package curso.entities;
 
 public class Product {
-    private final String name;
-    private final double price;
+    private String name;
+    private double price;
     private int quantity;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
 
     public Product(String name, double price, int quantity) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
     }
 
     public double totalValueInStock() {
