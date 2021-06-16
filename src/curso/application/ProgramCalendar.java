64c5211aa799c8d52ff8c940741c32d11906a7de
Junior Lima <juniiorliimatt@gmail.com>
2021-06-16 12:02:46 -0300

    trabalhando com calendar

---
+++ src/curso/application/ProgramCalendar.java
@@ -0,0 +1,26 @@
+package curso.application;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
+
+public class ProgramCalendar {
+    public static void main(String[] args) {
+        var sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+        System.out.println(sdf.format(d));
+
+        var cal = Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.HOUR_OF_DAY, 4);
+        d = cal.getTime();
+        System.out.println(d);
+
+        // meses começão a partir do 0;
+        var month = cal.get(Calendar.MONTH) + 1;
+        var minutes = cal.get(Calendar.MINUTE);
+        System.out.println("Mês: " + month + ", Minutes: " + minutes);
+        
+    }
+}
