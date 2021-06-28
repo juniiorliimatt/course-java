27add4c123cac4472e4ad378609537604d8caa85
Junior Lima <juniiorliimatt@gmail.com>
2021-06-28 20:39:54 -0300

    interfaces

--- src/curso/entities/Employee.java
+++ src/curso/entities/Employee.java
@@ -1,51 +1,73 @@
 package curso.entities;
 
-public class Employee {
-    private Integer id;
-    private final String name;
-    private Double grossSalary;
-    private Double tax;
+public class Employee implements Comparable<Employee> {
 
-    public Employee(Integer id, String name, Double grossSalary) {
-        this.id = id;
-        this.name = name;
-        this.grossSalary = grossSalary;
-    }
+  private Integer id;
+  private final String name;
+  private Double grossSalary;
+  private Double tax;
+  private Double salary;
 
-    public Employee(String name, Double grossSalary, Double tax) {
-        this.name = name;
-        this.grossSalary = grossSalary;
-        this.tax = tax;
-    }
+  public Employee(String name, Double salary) {
+    this.name = name;
+    this.salary = salary;
+  }
 
-    public double netSalary(){
-        return this.grossSalary - this.tax;
-    }
+  public Employee(Integer id, String name, Double grossSalary) {
+    this.id = id;
+    this.name = name;
+    this.grossSalary = grossSalary;
+  }
 
-    public void increaseSalary(Double percentage){
-        this.grossSalary += this.grossSalary * (percentage / 100);
-    }
+  public Employee(String name, Double grossSalary, Double tax) {
+    this.name = name;
+    this.grossSalary = grossSalary;
+    this.tax = tax;
+  }
 
-    public int getId() {
-        return id;
-    }
+  public double netSalary() {
+    return this.grossSalary - this.tax;
+  }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+  public void increaseSalary(Double percentage) {
+    this.grossSalary += this.grossSalary * (percentage / 100);
+  }
 
-    public String getName() {
-        return name;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public double getGrossSalary() {
-        return grossSalary;
-    }
+  public void setId(Integer id) {
+    this.id = id;
+  }
 
-    @Override
-    public String toString() {
-        return "Employee: " + this.name + ", $ " + this.netSalary();
-    }
+  public String getName() {
+    return name;
+  }
 
-    public String showInfo() { return getId() + ", " + getName() + ", " + String.format("%.2f", getGrossSalary()); }
+  public double getGrossSalary() {
+    return grossSalary;
+  }
+
+  public Double getSalary() {
+    return salary;
+  }
+
+  public void setSalary(Double salary) {
+    this.salary = salary;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee: " + this.name + ", $ " + this.netSalary();
+  }
+
+  public String showInfo() {
+    return getId() + ", " + getName() + ", " + String.format("%.2f", getGrossSalary());
+  }
+
+  @Override
+  public int compareTo(Employee o) {
+    return salary.compareTo(o.getSalary());
+  }
 }
---
+++ src/curso/services/UsaInterestService.java
@@ -0,0 +1,14 @@
+package curso.services;
+
+public class UsaInterestService implements InterestService {
+  private final double interestRate;
+
+  public UsaInterestService(double interestRate) {
+    this.interestRate = interestRate;
+  }
+
+  @Override
+  public double getInterestRate() {
+    return interestRate;
+  }
+}
