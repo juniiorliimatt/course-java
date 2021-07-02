8c2e126c71d2ea5796c59d20479f14f2681926cd
Junior Lima <juniiorliimatt@gmail.com>
2021-07-01 21:46:51 -0300

    main

---
+++ src/curso/application/Main.java
@@ -0,0 +1,12 @@
+package curso.application;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+  public static void main(String[] args) {
+    Map<String, String> set = new TreeMap<>();
+
+    set.put("name", "Junior");
+  }
+}
